d14e7132f4f6a047ccc4c783a139d7601d1814f0
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2023-03-12 20:26:12 +0700

    Change database

--- app/Work/Getitem.jsx
+++ app/Work/Getitem.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-// import supabase from '../../utils/supabase'
+import supabase from '../../utils/supabase'
 
-// export const revalidate = 10;
+export const revalidate = 10;
 
-// export const item = async ()=> {
-//     const {data} = await supabase.from("parks").select();
-//     return {data};
-// }
+export const item = async ()=> {
+    const {data} = await supabase.from("parks").select();
+    return {data};
+}
 
 const Getitem = async () => {
 
-  // const {data} = await item();
+  const {data} = await item();
 
   return (
     // <pre>{JSON.stringify(data, null, 2)}</pre>
     <div className='min-h-full p-2 mt-16'>
-        {/* {data.map((list) => (
+        {/* {{data.map((list) => (
             <p key={list.id}>
                 {list.title}
             </p>
-        ))} */}
-        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        ))}} */}
+        <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
